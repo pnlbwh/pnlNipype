cbe510732727e754be543f500611a72095821651
Billah, Tashrif <tbillah@bwh.harvard.edu>
2018-09-27 11:45:26 -0400

    axis alignment and centering

--- niftiAxisAlign.py
+++ niftiAxisAlign.py
@@ -12,36 +12,9 @@ with warnings.catch_warnings():
     import nibabel as nib
 
 from bvec_rotation import bvec_rotate
+from niftiCenter import centered_origin
 
 precision= 17
-
-def dot_product(v1, v2):
-    return [a*b for (a,b) in zip(v1, v2)]
-
-
-def centered_origin(hdr_in):
-
-    hdr_out= hdr_in.copy()
-
-    spc_dirs= hdr_in.get_sform()[0:3,0:3]
-
-    new_origin = []
-    for dir in spc_dirs:
-        new_size= [(x-1)/2  for x in hdr_in['dim'][1:4]]
-
-        dot_prod= dot_product(dir,new_size)
-        abs_dot_prod= [abs(x) for x in dot_prod]
-
-        max_elm= abs_dot_prod.index(max(abs_dot_prod))
-        new_origin.append(-dot_prod[max_elm])
-
-
-    hdr_out['qoffset_x'], hdr_out['qoffset_y'], hdr_out['qoffset_z']= new_origin
-    hdr_out['srow_x'][3], hdr_out['srow_y'][3], hdr_out['srow_z'][3]= new_origin
-
-    # save_image(mri_in.get_data(), hdr_out, out_prefix)
-
-    return hdr_out
 
 
 def get_spcdir_new(hdr_in):
@@ -146,14 +119,14 @@ class Xalign(cli.Application):
         ['--bvecs'],
         cli.ExistingFile,
         help='bvec file',
-        mandatory=True)
+        mandatory=False)
 
 
     bval_file= cli.SwitchAttr(
         ['--bvals'],
         cli.ExistingFile,
         help='bval file',
-        mandatory=True)
+        mandatory=False)
 
     out_prefix = cli.SwitchAttr(
         ['-o', '--outPrefix'],
@@ -235,6 +208,8 @@ if __name__ == '__main__':
 ~/Downloads/Dummy-PNL-nipype/niftiAxisAlign.py -i 1001-dwi.nii --bvals 1001-dwi.bval --bvecs 1001-dwi.bvec
 
 
+~/Downloads/Dummy-PNL-nipype/niftiAxisAlign.py -i 5006-dwi-shifted.nii --bvals 5006-dwi.bval --bvecs 5006-dwi.bvec --center
+
 DWIConvert
 cases=(3005 5006 7010);
 for i in ${cases[@]};
@@ -254,5 +229,9 @@ do
     ~/Downloads/Dummy-PNL-nipype/niftiAxisAlign.py -i 1001-dwi.nii --bvals dwi.bval --bvecs dwi.bvec.rot
     cd ..;
 done
+
+
+DWIConvert --inputVolume 5006-dwi-shifted.nrrd -o 5006-dwi-shifted.nii --outputBValues 5006-dwi-shifted.bval \
+--outputBVectors 5006-dwi-shifted.bvecs --conversionMode NrrdToFSL
 
 '''
