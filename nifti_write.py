5a3cb4cc8ca6d1ef8d44ea5b7caaba06d8e40928
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-01-13 23:05:16 -0500

    nrrd-->nifti conversion

---
+++ nifti_write.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import nrrd
+import numpy as np
+import argparse
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    import nibabel as nib
+
+PRECISION= 17
+np.set_printoptions(precision= PRECISION)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='NRRD to NIFTI conversion tool setting byteskip = -1')
+    parser.add_argument('-p', '--prefix', type=str, required=True,
+                        help='prefix for .nii.gz, .bval, and .bvec files')
+    parser.add_argument('-i', '--input', type=str, required=True, help='input nrrd file')
+
+    args = parser.parse_args()
+    prefix= args.prefix
+
+    img= nrrd.read(args.input)
+    hdr= img[1]
+    data= img[0]
+
+    axis_elements= hdr['kinds']
+    for i in range(4):
+        if axis_elements[i] == 'list' or axis_elements[i] == 'vector':
+            grad_axis= i
+            break
+
+    # put the gradients along last axis
+    if grad_axis!=3:
+        data= np.moveaxis(data, grad_axis, 3)
+
+    # automatically sets dim, data_type, pixdim, affine
+    xfrm= np.vstack((np.hstack((hdr['space directions'][:3,:3].T,
+                                np.reshape(hdr['space origin'],(3,1)))),[0,0,0,1]))
+    img_nifti= nib.nifti1.Nifti1Image(data, affine= xfrm)
+    hdr_nifti= img_nifti.header
+
+    # now set xyzt_units, sform_code= 1 (scanner)
+    # https://nifti.nimh.nih.gov/nifti-1/documentation/nifti1fields/nifti1fields_pages/xyzt_units.html
+    hdr_nifti.set_xyzt_units(xyz= 2, t= 8)
+    hdr_nifti['sform_code']= 1
+
+    nib.save(img_nifti, prefix+'.nii.gz')
+
+    # write .bval and .bvec
+    if hdr['dimension'] ==4:
+        f_val= open(prefix+'.bval', 'w')
+        f_vec= open(prefix+'.bvec', 'w')
+        b_max = float(hdr['DWMRI_b-value'])
+
+        for ind in range(hdr_nifti['dim'][4]):
+            bvec = [float(num) for num in hdr[f'DWMRI_gradient_{ind:04}'].split()]
+            L_2= np.linalg.norm(bvec)
+            bval= round(L_2 ** 2 * b_max)
+
+            if L_2:
+                bvec_norm= bvec/L_2
+            else:
+                bvec_norm= [0, 0, 0]
+
+            f_val.write(str(bval)+' ')
+            f_vec.write(('  ').join(str(x) for x in bvec_norm)+'\n')
+
+        f_val.close()
+        f_vec.close()
+
+
+
+if __name__ == '__main__':
+    main()
