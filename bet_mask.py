4705668b91f7224d043edb36fad659c0800690d7
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-06-02 21:26:46 +0000

    create single mask from 4D dwi

--- bet_mask.py
+++ bet_mask.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-from plumbum.cmd import bet
-from plumbum import cli, FG
+from plumbum.cmd import bet, fslroi
+from plumbum import cli, FG, local
 import os
+import numpy as np
+from bvec_rotation import read_bvals
 
 
 class App(cli.Application):
@@ -34,9 +36,23 @@ class App(cli.Application):
         if self.out is None:
             self.out= os.path.join(directory, prefix)
 
+        # bet changed in FSL 6.0.1, it creates a mask for every volume in 4D
+        # if the image is 4D, baseline image should be extracted first
 
-        bet[self.img, self.out, '-m', '-f', self.bet_threshold] & FG
+        bval_file = os.path.join(directory, prefix + '.bval')
+        bvals = read_bvals(bval_file)
+        idx = np.where([bval < 50 for bval in bvals])[0]
 
+        with local.tempdir() as tmpdir:
+            bsetmp= tmpdir / 'bse.nii.gz'
+
+            if len(idx) >= 1:
+                fslroi[self.img, bsetmp, idx, 1] & FG
+
+                bet[bsetmp, self.out, '-m', '-f', self.bet_threshold] & FG
+
+            else:
+                raise Exception('No b0 image found. Check the bval file.')
 
 if __name__ == '__main__':
     App.run()
