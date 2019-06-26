84b77fabd85d6e3f7b98e7071b58678cc81faa9e
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-06-26 11:58:33 -0400

    read eddy advanced params

--- util.py
+++ util.py
@@ -112,3 +112,20 @@ class TemporaryDirectory(object):
         if self._finalizer.detach():
             self._rmtree(self.name)
 
+
+
+# read the eddy configuration file:
+with open(pjoin(FILEDIR,'eddy_config.txt')) as f:
+    content= f.read().split('\n')
+    for line in content:
+
+        if '$ topup:' in line:
+            topup_params= line.split(':')[1]
+
+        elif '$ applytopup:' in line:
+            applytopup_params= line.split(':')[1]
+
+        elif '$ eddy_openmp:' in line:
+            eddy_openmp_params= line.split(':')[1]
+
+            
