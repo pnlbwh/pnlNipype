ccaf7ddf4ccf8a0e0d87c7f7c69dd2bff9a7e007
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-06-26 11:44:17 -0400

    masking for 3D/4D, account for FSL bet v6.0.1

--- bet_mask.py
+++ bet_mask.py
@@ -4,29 +4,57 @@ from plumbum.cmd import bet, fslroi
 from plumbum import cli, FG, local
 import os
 import numpy as np
-from bvec_rotation import read_bvals
+from conversion import read_bvals
+from util import BET_THRESHOLD, B0_THRESHOLD, load_nifti
+
+
+def bet_mask(imgPath, maskPath, dim, bvalFile= None, thr= BET_THRESHOLD):
+
+    with local.tempdir() as tmpdir:
+        bsetmp = tmpdir / 'bse.nii.gz'
+
+        if dim==4:
+            bvals = read_bvals(bvalFile)
+            idx = np.where([bval < B0_THRESHOLD for bval in bvals])[0]
+
+
+            if len(idx) >= 1:
+                fslroi[imgPath, bsetmp, idx, 1] & FG
+
+                bet[bsetmp, maskPath, '-m', '-n', '-f', thr] & FG
+
+            else:
+                raise Exception('No b0 image found. Check the bval file.')
+
+
+        elif dim==3:
+            bet[imgPath, maskPath, '-m', '-n', '-f', thr] & FG
+
+
+        else:
+            raise ValueError('Input dimension should be 3 or 4')
 
 
 class App(cli.Application):
-    """Extracts the brain mask of a 3D of 4D nifti image using fsl bet tool"""
+    """Extracts the brain mask of a 3D/4D nifti image using fsl bet command"""
 
     img = cli.SwitchAttr(
         ['-i', '--input'],
         cli.ExistingFile,
-        help='input 3D or 4D nifti image',
+        help='input 3D/4D nifti image',
         mandatory=True)
 
     out = cli.SwitchAttr(
-        ['-o', '--out'],
-        help='''prefix for output brain mask (default: input prefix).
-             output file is named as prefix_mask.nii.gz''',
+        ['-o', '--outPrefix'],
+        help='prefix for output brain mask (default: input prefix), output file is named as prefix_mask.nii.gz',
         mandatory=False)
 
     bet_threshold = cli.SwitchAttr(
         '-f',
         help= 'threshold for fsl bet mask',
         mandatory=False,
-        default= 0.25)
+        default= BET_THRESHOLD)
+
 
     def main(self):
 
@@ -39,20 +67,17 @@ class App(cli.Application):
         # bet changed in FSL 6.0.1, it creates a mask for every volume in 4D
         # if the image is 4D, baseline image should be extracted first
 
-        bval_file = os.path.join(directory, prefix + '.bval')
-        bvals = read_bvals(bval_file)
-        idx = np.where([bval < 50 for bval in bvals])[0]
+        dim= load_nifti(self.img._path).header['dim'][0]
 
-        with local.tempdir() as tmpdir:
-            bsetmp= tmpdir / 'bse.nii.gz'
+        if dim==4:
+            bvalFile = os.path.join(directory, prefix + '.bval')
+            bet_mask(self.img._path, self.out, 4, bvalFile, thr= self.bet_threshold)
 
-            if len(idx) >= 1:
-                fslroi[self.img, bsetmp, idx, 1] & FG
+        else:
+            bet_mask(self.img._path, self.out, 3, thr= self.bet_threshold)
 
-                bet[bsetmp, self.out, '-m', '-f', self.bet_threshold] & FG
 
-            else:
-                raise Exception('No b0 image found. Check the bval file.')
+
 
 if __name__ == '__main__':
     App.run()
