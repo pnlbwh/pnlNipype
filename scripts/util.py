4f64f5e8249753a3d6e692ca9c18f037d05fd701
Billah, Tashrif <tbillah@bwh.harvard.edu>
2021-08-12 14:27:30 -0400

    remove volatile _mask_name function

--- scripts/util.py
+++ scripts/util.py
@@ -115,20 +115,3 @@ class TemporaryDirectory(object):
         if self._finalizer.detach():
             self._rmtree(self.name)
 
-def _mask_name(mask_name, mask_qc=True):
-
-    qc_mask= local.path(mask_name.replace('_mask.nii.gz', 'Qc_mask.nii.gz'))
-    if 'To' not in mask_name:
-        # for MABS/CNN mask
-        if not qc_mask.exists():
-            print('\n\n** Quality checked mask not found\n'
-                  'You may want to check the quality of created mask {} . '
-                  'Once you are done, save the (edited) mask as {} .\n'
-                  'Continuing with the automated mask ... **\n\n'
-                  .format(mask_name, qc_mask))
-            return mask_name
-        else:
-            return qc_mask
-    else:
-        # for warped mask
-        return mask_name
